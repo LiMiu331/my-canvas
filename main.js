77d36c1c644f8c8fcce18b76c8817a49c9581094
libb <boomlibb1996@gmail.com>
2020-01-02 18:03:34 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var context = canvas.getContext('2d');
 //1.控制画板宽高
 autoSetCanvasSize(canvas)
 
-//2.监听用户鼠标行为
-listenToMouse(canvas)
+//2.监听用户行为
+listenToUser(canvas)
 
 //3.橡皮
 var usingEraser = false
@@ -44,48 +44,94 @@ function drawline(x1, y1, x2, y2) {
     context.stroke()
 }
 //监听用户函数
-function listenToMouse(canvasid) {
+function listenToUser(canvasid) {
     var using = false
 
-    //按下去鼠标
-    canvasid.onmousedown = function (down) {
-        var x = down.clientX
-        var y = down.clientY
-        using = true
-        if (usingEraser) {
-            context.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-            lastPonit = {
-                "x": x,
-                "y": y
-            }
-        }
-
-    }
-    //移动
-    canvasid.onmousemove = function (move) {
-        var x = move.clientX
-        var y = move.clientY
-
-        if (!using) { return }
-        
-        if (usingEraser) {
-            
+    //特性检测
+    if (document.body.ontouchstart !== undefined) {
+        //触屏设备
+        canvasid.ontouchstart = function (touchstart) {
+            var x = touchstart.touches[0].clientX
+            var y = touchstart.touches[0].clientY
+            console.log(x,y)
+            using = true
+            if (usingEraser) {
                 context.clearRect(x - 5, y - 5, 10, 10)
-          
-        } else {
-                NewPoint = {
+            } else {
+                lastPonit = {
                     "x": x,
                     "y": y
                 }
-            drawline(lastPonit.x, lastPonit.y, NewPoint.x, NewPoint.y)
-            lastPonit = NewPoint
+            }
+            
         }
-
+        canvasid.ontouchmove = function (touchmove) {
+            var x = touchmove.touches[0].clientX
+            var y = touchmove.touches[0].clientY
+    
+            if (!using) { return }
+            
+            if (usingEraser) {
+                
+                    context.clearRect(x - 5, y - 5, 10, 10)
+              
+            } else {
+                    NewPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                drawline(lastPonit.x, lastPonit.y, NewPoint.x, NewPoint.y)
+                lastPonit = NewPoint
+            }
+            console.log('边摸边动')
+        }
+        canvasid.ontouchend = function () {
+                using = false
+            console.log('摸完了')
+        }
+    } else {
+        //非触屏设备
+        canvasid.onmousedown = function (down) {
+            var x = down.clientX
+            var y = down.clientY
+            using = true
+            if (usingEraser) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                lastPonit = {
+                    "x": x,
+                    "y": y
+                }
+            }
+    
+        }
+        //移动
+        canvasid.onmousemove = function (move) {
+            var x = move.clientX
+            var y = move.clientY
+    
+            if (!using) { return }
+            
+            if (usingEraser) {
+                
+                    context.clearRect(x - 5, y - 5, 10, 10)
+              
+            } else {
+                    NewPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                drawline(lastPonit.x, lastPonit.y, NewPoint.x, NewPoint.y)
+                lastPonit = NewPoint
+            }
+    
+        }
+    
+        //松开
+        canvasid.onmouseup = function (up) {
+            using = false
+        }
     }
-
-    //松开
-    canvasid.onmouseup = function (up) {
-        using = false
-    }
+    
+    
 }
